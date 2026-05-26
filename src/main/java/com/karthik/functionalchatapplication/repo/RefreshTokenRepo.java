c22b374e48a9a4796aa2e2c810cdff0c4ef5cf4e
Karthik Narravula <nm6773083@gmail.com>
2026-05-26 16:42:57 +0530

    added query to refresh token repo

--- src/main/java/com/karthik/functionalchatapplication/repo/RefreshTokenRepo.java
+++ src/main/java/com/karthik/functionalchatapplication/repo/RefreshTokenRepo.java
@@ -3,6 +3,8 @@ package com.karthik.functionalchatapplication.repo;
 import com.karthik.functionalchatapplication.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +14,9 @@ import java.util.Optional;
 public interface RefreshTokenRepo extends JpaRepository<RefreshToken,Long> {
     Optional<RefreshToken> findByToken(String token);
 
-    @Transactional
+
+
     @Modifying
-    void deleteByUsername(String username);
-}
+    @Transactional
+    @Query("DELETE FROM RefreshToken r WHERE r.username = :username")
+    void deleteByUsername(@Param("username") String username);
